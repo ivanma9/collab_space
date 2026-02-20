bd063720c574d0e173003882e85ab48aa518b302
Ivan Ma <ivanma819@gmail.com>
2026-02-20 11:28:53 -0800

    feat: add TempConnectorLine component for in-progress connections

---
+++ src/components/canvas/TempConnectorLine.tsx
@@ -0,0 +1,24 @@
+import { Arrow } from "react-konva";
+
+interface TempConnectorLineProps {
+	fromPoint: { x: number; y: number };
+	toPoint: { x: number; y: number };
+}
+
+export function TempConnectorLine({
+	fromPoint,
+	toPoint,
+}: TempConnectorLineProps) {
+	return (
+		<Arrow
+			points={[fromPoint.x, fromPoint.y, toPoint.x, toPoint.y]}
+			stroke="#555555"
+			strokeWidth={2}
+			fill="#555555"
+			pointerLength={10}
+			pointerWidth={8}
+			dash={[6, 3]}
+			listening={false}
+		/>
+	);
+}
